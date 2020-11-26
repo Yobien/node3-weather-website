e6ebfd4aae3b74dc27cbbbf1fe192f90d05da77c
Timothee Brandt <brandt.timothee@gmail.com>
2020-11-26 11:23:38 +0100

    Hi

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('/weatther?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 return messageOne.textContent = data.error
