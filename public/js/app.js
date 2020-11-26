98e841ee80433816c7cb70b5e3294d1103a206dd
Timothee Brandt <brandt.timothee@gmail.com>
2020-11-26 11:13:37 +0100

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,7 @@
-console.log('Client side Javascript file is loaded')
-
-// fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             return console.log(data.error)
-//         }
-//         console.log(data.location)
-//         console.log(data.forecast)
-//     })
-// })
-
 const weatherForm = document.querySelector('form') // permet de target une banniere html
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1') // #message-1 permet de target l'ID message-1 du document HTML
 const messageTwo = document.querySelector('#message-2')
-
-// messageOne.textContent = 'From Javascript'
-
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault() // permet au browser de ne pas se refraichir apres une recherche, il "handle".
@@ -26,7 +11,7 @@ weatherForm.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weatther?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 return messageOne.textContent = data.error
